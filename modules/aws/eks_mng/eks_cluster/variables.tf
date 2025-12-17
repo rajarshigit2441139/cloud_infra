46f7325655576934277e06beb0ec2fea9f96d008
Rajarshi Mandal <rajarshimandal@Rajarshis-MacBook-Air.local>
2025-12-17 23:24:58 +0530

    made security_group_ids  optional

--- modules/aws/eks_mng/eks_cluster/variables.tf
+++ modules/aws/eks_mng/eks_cluster/variables.tf
@@ -4,7 +4,7 @@ variable "eks_clusters" {
     cluster_version         = string
     vpc_id                  = string
     subnet_ids              = list(string)
-    security_group_ids      = list(string)
+    security_group_ids      = optional(list(string))
     endpoint_public_access  = bool
     endpoint_private_access = bool
     tags                    = map(string)
