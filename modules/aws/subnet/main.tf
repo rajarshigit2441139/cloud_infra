40f00ce92a9c84a4dd2913911334597269e9a86d
Rajarshi Mandal <rajarshimandal@Rajarshis-MacBook-Air.local>
2026-01-08 23:38:42 +0530

    add map_public_ip_on_launch variable

--- modules/aws/subnet/main.tf
+++ modules/aws/subnet/main.tf
@@ -1,8 +1,9 @@
 resource "aws_subnet" "example" {
-  for_each   = var.subnet_parameters
-  vpc_id     = each.value.vpc_id
-  cidr_block = each.value.cidr_block
-  availability_zone = each.value.availability_zone
+  for_each                = var.subnet_parameters
+  vpc_id                  = each.value.vpc_id
+  cidr_block              = each.value.cidr_block
+  availability_zone       = each.value.availability_zone
+  map_public_ip_on_launch = each.value.map_public_ip_on_launch
   tags = merge(each.value.tags, {
     Name : each.key
   })
--- modules/aws/subnet/variables.tf
+++ modules/aws/subnet/variables.tf
@@ -1,12 +1,13 @@
 variable "subnet_parameters" {
   description = "Subnet parameters"
   type = map(object({
-    cidr_block = string
-    vpc_name   = string
-    vpc_id     = string
-    availability_zone = string
-    az_index   = number
-    tags       = optional(map(string), {})
+    cidr_block              = string
+    vpc_name                = string
+    vpc_id                  = string
+    availability_zone       = string
+    az_index                = number
+    map_public_ip_on_launch = optional(bool)
+    tags                    = optional(map(string), {})
   }))
   default = {}
 }
